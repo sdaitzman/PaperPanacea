182feb367cb23395593348c996bc9945eb66c023
jsieving <jane.sieving@students.olin.edu>
2019-12-03 16:38:52 -0500

    fixing button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,9 +8,9 @@
         <h1>What We Do</h1>
         <p>We provide citizen science kits that allow for in-home lead detection. Our kits are non-toxic and designed for ease of use, even by those with little or no chemistry background. Included in our kit are paper detection pads that are dosed with a chemical that will react with lead in water and darken color depending on how much lead there is. Using our app, you can analyze the photo and get an approximation of how much lead is in your water--as well as if it’s safe to drink or bathe with. We also provide citizens with resources on how to deal with contaminated water.</p>
       </div>
-      <div class="button">
-        <p>Read More<p>
-        </div>
+      <router-link to="/about/project">
+        <div class="button">Project</div>
+      </router-link>
     </div>
     <div class="map-container">
       <h2>Live Data Map</h2>
@@ -44,7 +44,6 @@ export default {
   padding: 10px 20px;
   margin-bottom: 30px;
 }
-
 
 .what-we-do p {
   font-family: "Open Sans", sans-serif;
